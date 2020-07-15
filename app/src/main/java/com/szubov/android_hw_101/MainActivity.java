9023b998e54c3d925372326550f3228e1d2e8d3a
Sergey Zubov <serjio.zubov@gmail.com>
2020-07-15 15:48:35 +0300

    for check

--- app/src/main/java/com/szubov/android_hw_101/MainActivity.java
+++ app/src/main/java/com/szubov/android_hw_101/MainActivity.java
@@ -1,35 +1,49 @@
 package com.szubov.android_hw_101;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.SimpleAdapter;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String KEY1 = "key1";
+    private final String KEY2 = "key2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView list = findViewById(R.id.list);
+        ListView mListView = findViewById(R.id.list);
 
-        String[] values = prepareContent();
-        BaseAdapter listContentAdapter = createAdapter(values);
-        list.setAdapter(listContentAdapter);
+        List<Map<String,String>> arrayList = prepareContent();
+        BaseAdapter listContentAdapter = createAdapter(arrayList);
+        mListView.setAdapter(listContentAdapter);
     }
 
     @NonNull
-    private BaseAdapter createAdapter(String[] values) {
-        return new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+    private BaseAdapter createAdapter(List<Map<String,String>> arrayList) {
+        return new SimpleAdapter(this, arrayList, R.layout.list_item,
+                new String[]{KEY1,KEY2}, new int[]{R.id.textView1, R.id.textView2});
     }
 
     @NonNull
-    private String[] prepareContent() {
-        return getString(R.string.large_text).split("\n\n");
+    private List<Map<String,String>> prepareContent() {
+        String[] arrayContent = getString(R.string.large_text).split("\n\n");
+        List<Map<String,String>> list = new ArrayList<>();
+        for (String s : arrayContent) {
+            Map<String, String> map = new HashMap<>();
+            map.put(KEY1, s);
+            map.put(KEY2, s.length() + "");
+            list.add(map);
+        }
+        return list;
     }
 }
